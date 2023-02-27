9084e569d403c453483d2509336a2c6c4ba60a0f
Anna Savinovskikh <8pipopolam@gmail.com>
2023-02-27 20:46:29 +0300

    write map frequency

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,25 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
+import (
+	"fmt"
+	"strings"
+)
+
+func Top10(text string) []string {
+	var stats map[string]int
+
+	for i, v := range strings.Split(text, " ") {
+		k := stats[v]
+		stats[v] = k + 1
+		fmt.Println(i, v)
+	}
+
+	a := make([]string, 10, 10)
+	var frequencies map[string]int
+
+	for i := 0; i < 10; i++ {
+		frequencies[stats[i]]
+	}
+
 	return nil
 }
